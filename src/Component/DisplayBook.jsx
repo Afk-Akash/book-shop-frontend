e2b82e3afb0ece25abaccc555a3c5633e48f5cb7
Akash Kumar <akashkumaraaa12@gmail.com>
2023-12-18 18:22:33 +0530

    TLE-04 : Akash/Mayur Add Book component

--- src/Component/DisplayBook.jsx
+++ src/Component/DisplayBook.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import './DisplayBook.css'
+import Book from './Book'
 
 const DisplayBook = ({all_books}) => {
+
+    
   return (
-    <div>
+    <div className='main-class'>
         {all_books.map((item, index) => {
             return (
-                <div>
-                    {item.name}
-                    {item.author}
-                    <img src={item.image} alt='book image'/>
+                <div className='main-div'>
+                    <img className = 'image-div' src={item.image} alt='book image was the'/>
+                    <div className='name-div'> {item.name} </div>
+                    <div className='desc-div'> {item.desc} </div>
+                    <div className='author-div'>Author: {item.author} </div>
+                    <div className='price-div' > {item.price} </div>
                 </div>
             )
         })}
