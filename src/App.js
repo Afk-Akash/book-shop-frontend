6e72a7aeb2e963569cafa1aded8915e48535513a
vedant.agarwal <vedant.agarwal@idfcfirstbank.com>
2023-12-19 11:43:41 +0530

    TLE:05 : Akash/Vedant : adding router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
-import all_books from "./Data/all_books";
-import DisplayBook from "./Component/DisplayBook";
+import './App.css';
+import all_books from './Data/all_books';
+import DisplayBook from './components/DisplayBook';
+import React from 'react';
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
+import Header from '../src/components/Header';
 
 function App() {
   return (
     <div className="App">
-      <DisplayBook all_books={all_books} />
+      <DisplayBook all_books = {all_books} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Header/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
