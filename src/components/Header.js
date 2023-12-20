8eb43b76383c4d5f3152206d9e8fdc8fc1c30411
Akash Kumar <akashkumaraaa12@gmail.com>
2023-12-20 10:44:21 +0530

    TLE : Akash/Vedant : add css for Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css"
 
 const Header = () => {
+
+
   return (
-    <div>
-      Header
-      <nav>
-        <ul>
-          <li>
-            <Link to="/signup">SignUp</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-      {/* Other header content */}
+    <div className="main-container">
+      <Link to='/home' style={{textDecoration:'none'}}>
+        <h1>Book Shop</h1>
+      </Link>
+      
+      <div className="login-signup">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/signup">SignUp</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
