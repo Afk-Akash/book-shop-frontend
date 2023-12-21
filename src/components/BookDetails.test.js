c3bacd9826b5915cc4dc8f296e51b6b7a87d2300
Akash Kumar <akashkumaraaa12@gmail.com>
2023-12-21 14:11:09 +0530

    TLE : Akash/Vedant : fix test case

--- src/components/BookDetails.test.js
+++ src/components/BookDetails.test.js
@@ -2,56 +2,38 @@ import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import BookDetails from "./BookDetails";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  MemoryRouter,
-} from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useParams: jest.fn().mockReturnValue({ id: '1' }), // Replace '1' with the desired book ID for testing
-// }));
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({ bookId: "123" }),
+}));
 
 describe("BookDetails Component", () => {
-  beforeEach(() => {
-    // Mock the useParams hook
-    jest.mock("react-router-dom", () => ({
-      ...jest.requireActual("react-router-dom"),
-      useParams: () => ({ id: "123" }),
-    }));
-  });
-
   test("renders book details", async () => {
     const mockBook = {
       bookName: "Sample Book",
       author: "John Doe",
       rating: 4.2,
       description: "This is a sample book description.",
-      price: "$19.99",
+      price: 19.99,
       numberOfAvailableBooks: 10,
       largeImageUrl: "sample-image.jpg",
     };
 
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({ books: mockBook }),
-      })
-    );
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockBook),
+    });
 
     render(
-      <MemoryRouter initialEntries={["/book/123"]}>
+      <MemoryRouter initialEntries={["/book/details/123"]}>
         <Routes>
-          <>
-            <Route path="/book/:id" element={<BookDetails />} />
-          </>
+          <Route path="/book/details/:bookId" element={<BookDetails />} />
         </Routes>
       </MemoryRouter>
     );
 
     await waitFor(() => {
-      // expect(screen.getByTestId('bookName')).toHaveTextContent(mockBook.bookName);
       expect(screen.getByText(`BY - ${mockBook.author}`)).toBeInTheDocument();
       expect(screen.getByText("4.2★")).toBeInTheDocument();
       expect(screen.getByText(mockBook.description)).toBeInTheDocument();
@@ -61,6 +43,9 @@ describe("BookDetails Component", () => {
           `Total Available Books - ${mockBook.numberOfAvailableBooks}`
         )
       ).toBeInTheDocument();
+      
     });
   });
+
+  // Add more test cases for different scenarios if needed
 });
