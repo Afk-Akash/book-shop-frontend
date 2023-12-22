0d42f1ce59e38bd50b636b914f55c4001cef39ee
Akash Kumar <akashkumaraaa12@gmail.com>
2023-12-22 15:42:23 +0530

    TLE : Akash : nothing much

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
-<<<<<<< Updated upstream
-
-=======
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
->>>>>>> Stashed changes
 import React, { useState } from "react";
 import all_books from "./Data/all_books";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Header from "../src/components/Header";
 import BookList from "./components/BookList";
 import SignUpPage from "./Component/SignUpPage";
 import LoginPage from "./Component/LoginPage";
 import BookDetails from "./components/BookDetails";
 import DeliveryDetailsPage from "./components/DeliveryDetailsPage";
 import OrderSummaryPage from "./components/OrderSummary";
-import Tokens from "./components/Tokens";
 
 function App() {
  
--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 
 const Tokens = ({ setAccessToken }) => {
     const [tokens, setTokens] = useState({});
